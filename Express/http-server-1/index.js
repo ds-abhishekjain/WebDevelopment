209666606390ce4e6507b81db841da087a6b1a5f
ds-abhishekjain <ds.abhishekjain@gmail.com>
2023-12-17 18:10:04 +0530

    First JS Project

---
+++ Express/http-server-1/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const port = 3000;
+const app = express();
+
+app.get('/',(req,res) => {
+    res.send('ALWAYS TAKE CARE OF WHAT YOU HAVE')
+})
+
+app.get('/data',(req,res) => {
+    res.json({
+        name: 'Abhishek',
+        age : 21
+    })
+})
+
+app.post('/conversation',(req,res) => {
+    console.log(req.body);
+    res.send({
+        msg : "2+2 =4"
+    })
+})
+
+
+app.listen(port,() => {
+    console.log(`Example app is listening port ${port}`);
+})
